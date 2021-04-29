7fe93288ab6125973f6d1523f775b3b4710a16ae
Mason Nguyen <masonnguyen18@gmail.com>
2021-04-28 22:58:18 -0700

    coords works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,30 @@
  *  @author Dat Nguyen
  */
 
-var i = document.getElementById('i-coordinate');
-var j = document.getElementById['j-coordinate'];
-var m = document.getElementById['m-coordinate'];
-var l = document.getElementById['l-coordinate'];
-console.log(i)
-console.log(j)
-console.log(m)
-console.log(l)
+ function validateInputs(input){
+    
+    if(isNaN(input) ){
+        return false;
+    }else if(input > 3 || input < 0){
+        return false;
+    }
+
+    return true;
+}
+
+ function validateForm(){
+    var i = document.forms["myForm"]["arg1"].value;
+    var j = document.forms["myForm"]["arg2"].value;
+    var m = document.forms["myForm"]["arg3"].value;
+    var l = document.forms["myForm"]["arg4"].value;
+    if(!validateInputs(i) || !validateInputs(j) || !validateInputs(m) || !validateInputs(l)){
+
+        alert("Inputs should be in range 0-3")
+        return false;
+    }
+
+    return true;
+
+}
+
+
--- src/views/elements/CoordinatesElement.php
+++ src/views/elements/CoordinatesElement.php
@@ -15,9 +15,9 @@ class CoordinatesElement extends Element {
 
     public function renderElement($i, $j, $m, $l) {
         ?>
-        <div class='coordinates' >
+        <div class='coordinates'>
         <span>Coordinates: </span>
-        <form>
+        <form method="POST" name="myForm" id='coordinates' onsubmit="return validateForm()">
             <div>
                 <label for='i-coordinate'><strong>i:  </strong></label>
                 <input id='i-coordinate' name="arg1" placeholder="0" type="text" />   
@@ -34,23 +34,12 @@ class CoordinatesElement extends Element {
                 <label for='l-coordinate'><strong>m:</strong></label>
                 <input id='l-coordinate' name="arg4" placeholder="0" type="text" />   
             </div>
+            <div>
+                <input type="submit" value="GO">
+            </div>
+
         </form>
+        
         <?php
-        if (isset($i) && isset($j) && isset($m) && isset($l)) { // all inputs
-            ?>
-                <a href='index.php?c=MapController&m=show_map&arg1=<?= $i ?>&arg2=<?= $j ?>&arg3=<?= $m ?>&arg4=<?= $l ?>'><button>GO</button></a>
-            <?php
-        } 
-        else if (isset($i) && isset($j)) { // i and j inputs
-            ?>
-                <a href='index.php?c=MapController&m=show_map&arg1=<?=$i ?>&arg2=<?= $j ?>'><button>GO</button></a>
-            <?php
-        } 
-        else { // no inputs
-            ?>
-                <a href='index.php?c=MapController&m=show_map'><button>GO</button></a>
-            <?php
-        }
-        ?></div><?php
     }
 }
