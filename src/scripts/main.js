57ed27d28b45aac81f65eb6980c0b284f449702e
Mason Nguyen <masonnguyen18@gmail.com>
2021-04-29 23:27:39 -0700

    fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,25 +3,32 @@
  *  @author Dat Nguyen
  */
 
- function validateInputs(input){
-    
-    if(isNaN(input) ){
-        return false;
-    }else if(input > 3 || input < 0){
+ function validateInput(input){
+    input = Number(input)
+    if(isNaN(input)){
         return false;
     }
+    else if(!Number.isInteger(input)){
+        return false;
+    }
+    else if(input > 3 || input < 0){
+        return false;
+    }
+    else{
+        return true;
+    }
+    
 
-    return true;
 }
 
  function validateForm(){
-    var i = document.forms["myForm"]["arg1"].value;
-    var j = document.forms["myForm"]["arg2"].value;
-    var m = document.forms["myForm"]["arg3"].value;
-    var l = document.forms["myForm"]["arg4"].value;
-    if(!validateInputs(i) || !validateInputs(j) || !validateInputs(m) || !validateInputs(l)){
+    var i = document.forms['myForm']['arg1'].value;
+    var j = document.forms['myForm']['arg2'].value;
+    var m = document.forms['myForm']['arg3'].value;
+    var l = document.forms['myForm']['arg4'].value;
+    if(!validateInput(i) || !validateInput(j) || !validateInput(m) || !validateInput(l)){
 
-        alert("Inputs should be in range 0-3")
+        alert("Inputs should be integers in range 0-3")
         return false;
     }
 
